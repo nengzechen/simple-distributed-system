ab46a23e086173411a997e1b1feaf12ecfb9abc8
hisky <c919670942@gmail.com>
2025-01-06 17:06:39 +0800

    feature: 创建了日志服务

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # simple-distrubuted-system
+使用go构建简单的分布式系统
+## 服务注册
+- 创建web服务
+- 创建注册服务
+- 注册web服务
+- 取消注册web服务
+## 服务发现
+## 状态检测
---
+++ cmd/logservice/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	stlog "log"
+	"sds/log"
+	"sds/service"
+)
+
+func main() {
+	log.Run("./distributed.log")
+	host, port := "localhost", "4000"
+	ctx, err := service.Start(
+		context.Background(),
+		"Log Server",
+		host,
+		port,
+		log.RegisterHandlers,
+	)
+	if err != nil {
+		stlog.Fatalln(err)
+	}
+	<-ctx.Done()
+	fmt.Println("shutting down service")
+}
